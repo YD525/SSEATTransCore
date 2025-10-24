09c7e007e7b16f0a1dbb8d6646d2bfc838258f0a
YD525 <525084464@qq.com>
2025-10-24 12:49:25 +0800

    UPDate

---
+++ SSEATTransCore/SkyrimManagement/SkyrimHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSEATTransCore.SkyrimManagement
+{
+    public class SkyrimHelper
+    {
+        public static bool FindPapyrusCompilerPath(ref string CompilerPathPtr)
+        {
+            if (File.Exists(DeFine.GetFullPath(@"Tool\Original Compiler\PapyrusAssembler.exe")))
+            {
+                CompilerPathPtr = DeFine.GetFullPath(@"Tool\Original Compiler\PapyrusAssembler.exe");
+                return true;
+            }
+            if (Directory.Exists(DeFine.GlobalLocalSetting.SkyrimPath))
+            {
+                if (!DeFine.GlobalLocalSetting.SkyrimPath.EndsWith(@"\"))
+                {
+                    DeFine.GlobalLocalSetting.SkyrimPath += @"\";
+                }
+                string SetPapyrusAssemblerPath = DeFine.GlobalLocalSetting.SkyrimPath + "Papyrus Compiler" + @"\PapyrusAssembler.exe";
+                if (File.Exists(SetPapyrusAssemblerPath))
+                {
+                    CompilerPathPtr = SetPapyrusAssemblerPath;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
